dbcf8d5ef67f721a6a0badaabd93efca01d6b174
liutingjieni <email@example.com>
2019-06-02 16:53:35 +0800

    创建新进程

--- fork.c
+++ fork.c
@@ -2,26 +2,41 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
+
+int globVar=5;
+
 int main()
 {
     pid_t pid;
-
+    int var=1,i;
 
     printf("Process Creation Study\n");
-    pid=fork();
+    pid=vfork();
     switch(pid)
     {
         case 0:
-            while(1)
+            i=3;
+            while(i-->0)
             {
-                printf("A background process is running ,pid is %d,Parentpid is %d\n",getpid(),getppid());
-                sleep(3);
+                printf("Child process is running\n");
+                globVar++;
+                var++;
+                sleep(1);
             }
+            printf("Child's globVar =%d,var=%d\n",globVar,var);
+            break;
         case -1:
-            perror("Process creation failed\n");
-            exit(-1);
+            perror("Process creation failed");
         default:
-            printf("Parent process is running ,Parentpid is %d\n",getpid());
+            i=5;
+            while(i-->0)
+            {
+                printf("Parent process is running\n");
+                globVar++;
+                var++;
+                sleep(1);
+            }
+            printf("Parent's globVar =%d,var=%d\n",globVar,var);
             exit(0);
     }
 
